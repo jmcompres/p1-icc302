86274a27cde484db55eff1cdb941daca6d0a6634
jmcompres <jmca0001@ce.pucmm.edu.do>
2026-02-18 13:45:06 -0400

    Implementando manejo de errores

--- evaluador.hs
+++ evaluador.hs
@@ -4,17 +4,53 @@ data Expr a = Lit a                                -- Lit de literal (cambié de
           | Sub (Expr a) (Expr a)
           | Mul (Expr a) (Expr a)
           | Div (Expr a) (Expr a)
-          | Exp (Expr a) (Expr a)
+          | Pow (Expr a) (Expr a)
           | Log (Expr a) (Expr a)
           deriving (Show, Eq)
 
-eval :: (Floating a) => Expr a -> a
-eval (Lit x) = x
-eval (Add e1 e2) = (eval e1) + (eval e2)
-eval (Sub e1 e2) = (eval e1) - (eval e2)
-eval (Mul e1 e2) = (eval e1) * (eval e2)
-eval (Div e1 e2) = (eval e1) / (eval e2)
-eval (Exp e1 e2) = (eval e1) ** (eval e2)          -- ** Es para elevar flotantes
-eval (Log e1 e2) = logBase (eval e1) (eval e2)
 
--- eval (Log (Div (Lit 90) (Mul (Exp (Lit 3) (Lit 2)) (Lit 5))) (Lit 8)) -> Ejemplo
+
+-- Función de evaluación recursiva
+eval :: (Floating a, Eq a, Ord a) => Expr a -> Maybe a
+eval (Lit x) = Just x
+eval (Add e1 e2) = sadd (eval e1) (eval e2)
+eval (Sub e1 e2) = ssub (eval e1) (eval e2)
+eval (Mul e1 e2) = smul (eval e1) (eval e2)
+eval (Div e1 e2) = sdiv (eval e1) (eval e2)
+eval (Pow e1 e2) = spow (eval e1) (eval e2)          -- ** Es para elevar flotantes
+eval (Log e1 e2) = slog (eval e1) (eval e2)
+
+-- eval (Log (Div (Lit 90) (Mul (Pow (Lit 3) (Lit 2)) (Lit 5))) (Lit 8)) -> Ejemplo
+
+
+
+-- Funciones auxiliares (no recursivas) para el manejo de errores (No se pueden sumar/restar/dividir/etc. dos justs en las eval)
+-- (s de safe)
+
+sadd :: (Floating a, Eq a, Ord a) => Maybe a -> Maybe a -> Maybe a
+sadd (Just x) (Just y) = Just (x+y)
+sadd _ _ = Nothing
+
+ssub :: (Floating a, Eq a, Ord a) => Maybe a -> Maybe a -> Maybe a
+ssub (Just x) (Just y) = Just (x-y)
+ssub _ _ = Nothing
+
+smul :: (Floating a, Eq a, Ord a) => Maybe a -> Maybe a -> Maybe a
+smul (Just x) (Just y) = Just (x*y)
+smul _ _ = Nothing
+
+sdiv :: (Floating a, Eq a, Ord a) => Maybe a -> Maybe a -> Maybe a
+sdiv (Just _) (Just 0) = Nothing
+sdiv (Just x) (Just y) = Just (x/y)
+sdiv _ _ = Nothing
+
+spow :: (Floating a, Eq a, Ord a) => Maybe a -> Maybe a -> Maybe a
+spow (Just x) (Just y) = Just (x**y)
+spow _ _ = Nothing
+
+slog :: (Floating a, Eq a, Ord a) => Maybe a -> Maybe a -> Maybe a
+slog (Just x) (Just y)
+    | (x<=0) = Nothing
+    | (x==1) = Nothing
+    | (y<=0) = Nothing
+slog _ _ = Nothing
